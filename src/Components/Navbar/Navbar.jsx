5e413de7163362cb8e2677048819775338396aed
Imon <contact@imonweb.co.uk>
2024-09-02 22:33:31 +0100

    video player functionality

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.scss';
 import logo from '../../assets/logo.png';
+import { Link } from 'react-scroll';
+import menu_icon from '../../assets/menu-icon.png';
 
 const Navbar = () => {
 
@@ -12,17 +14,71 @@ const Navbar = () => {
     })
   }, []);
 
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true); 
+  }
+
   return (
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
       <img src={logo} alt="" className='logo'/>
-      <ul>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About Us</li>
-        <li>Campus</li>
-        <li>Testimonials</li>
-        <li> <button className='btn'>Contact Us</button></li>
+      <ul className={mobileMenu ? "" : 'hide-mobile-menu' }>
+        <li><Link 
+              to='hero'
+              smooth='true'
+              offset={0}
+              duration={500}
+            >Home</Link>
+        </li>
+        <li>
+          <Link 
+              to='program'
+              smooth='true'
+              offset={0}
+              duration={-260}
+            >Program</Link>
+        </li>
+        <li>
+          <Link 
+              to='about'
+              smooth='true'
+              offset={0}
+              duration={-150}
+            >About Us</Link>
+        </li>
+        <li>
+          <Link 
+              to='campus'
+              smooth='true'
+              offset={0}
+              duration={-260}
+            >Campus</Link>
+        </li>
+        <li>
+          <Link 
+              to='testimonials'
+              smooth='true'
+              offset={0}
+              duration={-260}
+            >Testimonials</Link>
+        </li>
+        <li> <button className='btn'>
+            <Link 
+              to='contact'
+              smooth='true'
+              offset={0}
+              duration={500}
+            >Contact Us</Link>  
+          </button>
+        </li>
       </ul>
+      <img 
+        src={menu_icon} 
+        className="menu-icon" 
+        alt="" 
+        onClick={toggleMenu}
+      />
     </nav>
   )
 }
